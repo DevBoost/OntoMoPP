508805f1954c15656375beab679ad678ccccf49e
Christian Wende <christian.wende@devboost.de>
2010-10-11 11:29:11 +0000

    fixed reference resolver

--- org.emftext.language.owl.resource.owl/src/org/emftext/language/owl/resource/owl/analysis/FeatureReferenceFeatureReferenceResolver.java
+++ org.emftext.language.owl.resource.owl/src/org/emftext/language/owl/resource/owl/analysis/FeatureReferenceFeatureReferenceResolver.java
@@ -6,6 +6,8 @@
  */
 package org.emftext.language.owl.resource.owl.analysis;
 
+import org.emftext.language.owl.resource.owl.analysis.custom.CrossResourceIRIResolver;
+
 public class FeatureReferenceFeatureReferenceResolver
 		implements
 		org.emftext.language.owl.resource.owl.IOwlReferenceResolver<org.emftext.language.owl.FeatureReference, org.emftext.language.owl.Feature> {
@@ -19,14 +21,19 @@ public class FeatureReferenceFeatureReferenceResolver
 			int position,
 			boolean resolveFuzzy,
 			final org.emftext.language.owl.resource.owl.IOwlReferenceResolveResult<org.emftext.language.owl.Feature> result) {
+		CrossResourceIRIResolver.theInstance()
+				.doResolve(identifier, container, resolveFuzzy, result,
+						org.emftext.language.owl.Feature.class);
 		delegate.resolve(identifier, container, reference, position,
 				resolveFuzzy, result);
+
 	}
 
 	public java.lang.String deResolve(org.emftext.language.owl.Feature element,
 			org.emftext.language.owl.FeatureReference container,
 			org.eclipse.emf.ecore.EReference reference) {
-		return delegate.deResolve(element, container, reference);
+		return CrossResourceIRIResolver.theInstance().deResolve(element,
+				container, reference);
 	}
 
 	public void setOptions(java.util.Map<?, ?> options) {
