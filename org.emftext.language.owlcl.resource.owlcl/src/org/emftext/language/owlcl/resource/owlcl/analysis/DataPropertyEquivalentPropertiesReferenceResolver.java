b8199bef50a6fd250e66a7d5c4ecabaeac0ce4fe
Mirko Seifert <mirko.seifert@devboost.de>
2010-11-03 19:37:52 +0000

    regenerated delegating reference resolvers remove some empty packages

--- org.emftext.language.owlcl.resource.owlcl/src/org/emftext/language/owlcl/resource/owlcl/analysis/DataPropertyEquivalentPropertiesReferenceResolver.java
+++ org.emftext.language.owlcl.resource.owlcl/src/org/emftext/language/owlcl/resource/owlcl/analysis/DataPropertyEquivalentPropertiesReferenceResolver.java
@@ -1,9 +1,17 @@
-/**
- * <copyright>
- * </copyright>
- *
+/*******************************************************************************
+ * Copyright (c) 2006-2010 
+ * Software Technology Group, Dresden University of Technology
  * 
- */
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany 
+ *      - initial API and implementation
+ ******************************************************************************/
+
 package org.emftext.language.owlcl.resource.owlcl.analysis;
 
 public class DataPropertyEquivalentPropertiesReferenceResolver implements org.emftext.language.owlcl.resource.owlcl.IOwlclReferenceResolver<org.emftext.language.owl.DataProperty, org.emftext.language.owl.DataProperty> {
@@ -51,6 +59,35 @@ public class DataPropertyEquivalentPropertiesReferenceResolver implements org.em
 			
 			public void addMapping(String identifier, org.emftext.language.owl.DataProperty target, String warning) {
 				result.addMapping(identifier, target, warning);
+			}
+			
+			public java.util.Collection<org.emftext.language.owl.resource.owl.IOwlQuickFix> getQuickFixes() {
+				return java.util.Collections.emptySet();
+			}
+			
+			public void addQuickFix(final org.emftext.language.owl.resource.owl.IOwlQuickFix quickFix) {
+				result.addQuickFix(new org.emftext.language.owlcl.resource.owlcl.IOwlclQuickFix() {
+					
+					public String getImageKey() {
+						return quickFix.getImageKey();
+					}
+					
+					public String getDisplayString() {
+						return quickFix.getDisplayString();
+					}
+					
+					public java.util.Collection<org.eclipse.emf.ecore.EObject> getContextObjects() {
+						return quickFix.getContextObjects();
+					}
+					
+					public String getContextAsString() {
+						return quickFix.getContextAsString();
+					}
+					
+					public String apply(String currentText) {
+						return quickFix.apply(currentText);
+					}
+				});
 			}
 		});
 		
